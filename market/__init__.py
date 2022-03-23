c92295ecc1efaa436edf5aa1de9ae7085d6fa14b
Mukund Khandelwal <pythondev@ongraph.com>
2022-03-23 19:28:47 +0530

    updated forms and restructure

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'dab701b0ba1d23c54a1b926c'
 db = SQLAlchemy(app)
 
 from market import routes
